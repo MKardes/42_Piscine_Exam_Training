c9f7edebc9d5335303a86db266bb1d56af3da8c8
Musab <msivas5858@gmail.com>
2021-12-11 00:03:15 +0300

    Reihen

--- Level_2/ft_atoi/ft_atoi.c
+++ Level_2/ft_atoi/ft_atoi.c
@@ -2,26 +2,20 @@ int     ft_atoi(const char *str)
 {
         int     i;
         int     temp;
+	int	a;
 
+	a = 1;
         temp = 0;
         i = 0;
-        while (str[i] <= 13 && str[i] >= 7)
+        while (str[i] <= 13 && str[i] >= 9 || str[i] == ' ')
                 i++;
-        if (str[i] == '-' || str[i] '+')
+	if (str[i] == '-' || str[i] == '+')
                 {
                         if (str[i] == '-')
-                               temp*=-1;
+                               a = -1;
                         i++;
                 }
-        while (str[i] >= '0' && str[i] <= '9')
-                temp = temp * 10 + str[i] - 48;
-        return(temp);
-
-}
-
-#include <stdio.h>
-int     main()
-{
-        char *asd = " 1213a12";
-        printf("%d",ft_atoi(asd));
+	while (str[i] != '\0' && str[i] >= '0' && str[i] <= '9')
+                temp = temp * 10 + str[i++] - 48;
+        return(temp * a);
 }
